395ee7af765acc4241072bf434cb2349756ecd11
Vashu Nayak <97188321+Vashu-Nayak@users.noreply.github.com>
2024-11-20 19:32:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
+	let table=document.getElementById('sampleTable')
+	let row=table.insertRow(0);
+	let col1=row.insertCell(0)
+	let col2=row.insertCell(1)
+	col1.innerText='New Cell1'
+	col2.innerText='New Cell2'
+	
   
   
 }
